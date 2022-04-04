0e4a054d2c392c5e0d04da224e998c337f103b0f
jpucholv <96960258+jpucholv@users.noreply.github.com>
2022-04-04 22:28:43 +0200

    Ejercicio_5_1_completo

---
+++ Ejercicio_5_1.py
@@ -0,0 +1,11 @@
+from math import pi
+
+def areaTriangulo (base, altura):
+    return (base*altura)/2
+
+print(areaTriangulo(5,4))
+
+def areaCirculo (radio):
+    return pi * radio**2
+
+print(areaCirculo(5))
